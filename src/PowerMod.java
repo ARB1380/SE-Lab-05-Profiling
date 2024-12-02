7012a07da6a07f9742c534836df5e3bd73dab94d
ARB1380 <azerila.farshi@gmail.com>
2024-12-02 22:36:28 +0330

    enable profiling

--- src/PowerMod.java
+++ src/PowerMod.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
 
 public class PowerMod {
 
     public static void main(String[] args) {
-        int a = 99102026;
-        int power = 99101268;
-        int mod = 1000000007;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press number1: ");
+        scanner.nextInt();
+        System.out.println("Press number2: ");
+        scanner.nextInt();
+        System.out.println("Press number3: ");
+        scanner.nextInt();
+
+        long a = 99102026;
+        long power = 991012613;
+        long mod = 1000000007;
 
         System.out.println("Power Calculated! Result: " + power(a, power, mod));
     }
 
-    public static long power(int a, int power, int mod) {
+    public static long power(long a, long power, long mod) {
         long result = 1;
-        for (int i = 0; i < power; i++) {
+        for (long i = 0; i < power; i++) {
             result *= a;
             result %= mod;
         }
